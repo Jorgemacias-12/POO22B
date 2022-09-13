d651df0931f066f7d34bcb134fa1d8c86fa318b5
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-09-13 02:17:49 -0500

    Add - utilerias de color
    	He añadido esta clase para utileria de color, proximamente
    	tal vez existan otras clases de utileria.

---
+++ src/Utils/ColorUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO22B_MZJA.src.Utils
+{
+    public class ColorUtils
+    {
+
+        private static readonly Random Rand = new Random();
+        private static readonly object syncObject = new object();
+
+        public static Color GetRandomColor()
+        {
+            lock (syncObject)
+            {
+                return Color.FromArgb(Rand.Next(0, 256), Rand.Next(0, 256), Rand.Next(0, 256));
+            }
+        }
+    }
+}
