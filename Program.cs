053204ffef641b4f5ed7a1d4886626e4d81aedbc
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-09-13 02:12:29 -0500

    Add - Modificaciones al proyecto
    	He movido y renombrado los archivos Form1.cs y designer.
    
    	Al mismo tiempo ha sido cambiada la referencia de la clase en
    	el archivo Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace POO22B_MZJA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DlgPrincipal());
         }
     }
 }
