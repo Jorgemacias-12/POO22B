5b91ec72642a4232dd1f59192ffc308571e6795a
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-09-15 11:29:47 -0500

    Add - Estructurando clase
    	Agregando comportamientos y propiedades en
    	el constructor

--- src/Clases/CParticula.cs
+++ src/Clases/CParticula.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using POO22B_MZJA.src.Utils;
 using System.Windows.Forms;
 
 namespace POO22B_MZJA.src.Clases
@@ -20,15 +13,103 @@ namespace POO22B_MZJA.src.Clases
         // +------------------------------------------------------------------+
         // | Atributos                                                        |   
         // +------------------------------------------------------------------+
+        private int XBase;
+        private int YBase;
+        private int Altura;
         public int Velocidad;
+        private bool Norte;
+        private bool Sur;
+        private bool Este;
+        private bool Oeste;
+        private Control Base;
 
         // +------------------------------------------------------------------+
         // | Constructor                                                      |   
         // +------------------------------------------------------------------+
-        public CParticula(int velocidad) : base()
+        public CParticula(int XBase, int YBase, Control Base) : base()
         {
             InitializeComponent();
-            Velocidad = velocidad;
+
+            this.XBase = XBase;
+            this.YBase = YBase;
+            this.Base = Base;
+
+            Altura = 0;
+            Norte = false;
+            Sur = true;
+            Este = true;
+            Oeste = false;
+            Velocidad = 2;
+
+            BackColor = ColorUtils.GetRandomColor();
+            Location = new System.Drawing.Point(XBase, YBase);
+            Size = new System.Drawing.Size(32, 32);
+
+            Base.Controls.Add(this);
+            this.BringToFront();
+
+        }
+
+        // +------------------------------------------------------------------+
+        // | Comienza el funcionamiento del dron     |   
+        // +------------------------------------------------------------------+
+        public void Enciende()
+        {
+            // TODO
+        }
+
+        // +------------------------------------------------------------------+
+        // | Eleva la particula                                               |   
+        // +------------------------------------------------------------------+
+        public void Eleva(int Altura)
+        {
+
+        }
+
+        // +------------------------------------------------------------------+
+        // | Desplaza la particula cambiando rumbo y velocidad                |   
+        // +------------------------------------------------------------------+
+        public void Desplaza(bool Norte, bool Sur, bool Este, bool Oeste, int Velocidad)
+        {
+            this.Norte = Norte;
+            this.Sur = Sur;
+            this.Este = Este;
+            this.Oeste = Oeste;
+            this.Velocidad = Velocidad;
+        }
+
+        // +------------------------------------------------------------------+
+        // | Cambiar rumbo de la particula                                    |   
+        // +------------------------------------------------------------------+
+        public void Desplaza(bool Norte, bool Sur, bool Este, bool Oeste)
+        {
+            this.Norte = Norte;
+            this.Sur = Sur;
+            this.Este = Este;
+            this.Oeste = Oeste;
+        }
+
+        // +------------------------------------------------------------------+
+        // | Cambiar la velocidad de la particula                             |   
+        // +------------------------------------------------------------------+
+        public void Desplaza(int Velocidad)
+        {
+            this.Velocidad = Velocidad;
+        }
+
+        // +------------------------------------------------------------------+
+        // | Deshabilita las operaciones de la particula                      |   
+        // +------------------------------------------------------------------+
+        public bool Apagar()
+        {
+
+            if (Altura == 0)
+            {
+                return true;
+            }
+
+            return false;
+
         }
 
     }
