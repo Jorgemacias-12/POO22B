07e0eb826738743b11748a673ddd87561ae36f7f
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-10-05 11:50:10 -0500

    Add: Generar clases NC15
    	He generado codigo para las clases, en el proximo commit
    	habrá implementación de la clase Animal

---
+++ src/Clases/CHomoSapien.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO22B_MZJA.src.Clases
+{
+    // +------------------------------------------------------------------+
+    // |  Clase que representa un Homo Sapiens                             |
+    // |  MZJA 29/09/22.                                                  |
+    // +------------------------------------------------------------------+
+    public class CHomoSapiens : CHominidae
+    {
+
+        // +------------------------------------------------------------------+
+        // |  Atributos                                                       |
+        // +------------------------------------------------------------------+
+
+
+        // +------------------------------------------------------------------+
+        // |  Constructor                                                     |
+        // +------------------------------------------------------------------+
+        public CHomoSapiens() : base()
+        {
+
+        }
+
+    }
+}
---
+++ src/Clases/CSerVivo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO22B_MZJA.src.Clases
+{
+    // +------------------------------------------------------------------+
+    // |  Clase que representa un ser vivo                                |
+    // |  MZJA 29/09/22.                                                  |
+    // +------------------------------------------------------------------+
+    public class CSerVivo : UserControl
+    {
+        // TODO: implementar método de movimiento en clase Animal
+        
+        /*
+         * 1 -> DlgPractica 4
+         * 2 -> CSerVivo
+         * 
+         * Clase Animal
+         * 
+         */
+
+        // +------------------------------------------------------------------+
+        // |  Atributos                                                       |
+        // +------------------------------------------------------------------+
+
+        // Atributos de la base
+        private int XNacimiento;
+        private int YNacimiento;
+
+        // Atributos del áre de desplazamiento
+        private Control AreaDesplazamiento;
+
+        private bool Norte;
+        private bool Sur;
+        private bool Este;
+        private bool Oeste;
+        private int Velocidad;
+
+        // Atributos de control
+        private bool Nace;
+        private bool RegresarACasa;
+
+        // Atributos de ejecución
+        private Thread ProcesoVida;
+
+
+        // +------------------------------------------------------------------+
+        // |  Constructor                                                     |
+        // +------------------------------------------------------------------+
+        public CSerVivo()
+        {
+
+        }
+
+    }
+}
