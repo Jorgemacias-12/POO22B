a78d239fe2627ece8afdddeb3a2b6524401a0c7c
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-10-07 05:55:01 -0500

    Add - Utility classes
    	Agregando función GetColorShade en ColorUtils
    
    	Añadir método para generar booleanos de
    	forma aleatoria (pseudo)

--- src/Utils/Color/ColorUtils.cs
+++ src/Utils/Color/ColorUtils.cs
@@ -21,28 +21,36 @@ namespace POO22B_MZJA.src.Utils
                 return Color.FromArgb(Rand.Next(0, 256), Rand.Next(0, 256), Rand.Next(0, 256));
             }
         }
-       
+
 
         public static Color GenerateColorShade(string ColorName)
         {
             const int MAX = 150;
-            const int MIN = 100;
+            const int MIN = 20;
+            const int OFFSET = 1;
 
+            int generatedNumber;
             Color ShadeColor;
 
+            // Fallback color
+            ShadeColor = Color.White;
 
+            generatedNumber = (int)Math.Floor(Rand.NextDouble() * (MAX - MIN + OFFSET)) + MIN;
 
-            switch(ColorName)
+            switch (ColorName)
             {
-                case "green":
-                    break;
                 case "red":
+                    ShadeColor = Color.FromArgb(generatedNumber, 0, 0);
+                    break;
+                case "green":
+                    ShadeColor = Color.FromArgb(0, generatedNumber, 0);
                     break;
                 case "blue":
+                    ShadeColor = Color.FromArgb(0, 0, generatedNumber);
                     break;
             }
 
-            return Color.White;
+            return ShadeColor;
         }
 
     }
---
+++ src/Utils/ColorBase/ShadeGenereratorBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO22B_MZJA.src.Utils.Color
+{
+    public abstract class ShadeGenereratorBase
+    {
+        public abstract void GenerateShade(string ColorName);
+    }
+}
