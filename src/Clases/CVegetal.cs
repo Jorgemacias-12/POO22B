0986b5e32303a47de00694577e470dc071fb31ad
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-10-09 15:42:46 -0500

    Feat - Implementación terminada #5
    	He terminado la implementación de la clase
    	vegetal.
    
    	closes #5

--- src/Clases/CVegetal.cs
+++ src/Clases/CVegetal.cs
@@ -1,5 +1,4 @@
 ﻿using POO22B_MZJA.src.Utils;
-using POO22B_MZJA.src.Utils.Rng;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -7,43 +6,49 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
 namespace POO22B_MZJA.src.Clases
 {
     public class CVegetal : CSerVivo
     {
 
         private Point CrecimientoMaximo;
-        private Random random = new Random();
+        private readonly Random random;
         private bool Crecido;
         private int TasaCrecimiento;
 
         public CVegetal(Control AreaDesplazamiento, int XNacimiento, int YNacimiento) :
             base(AreaDesplazamiento, XNacimiento, YNacimiento)
         {
+            random = new Random();
+            // Eliminar borde
+            FlatAppearance.BorderSize = 0;
+
+            // Definir crecimiento máximo del vegetal
+            CrecimientoMaximo.X = random.Next(1, 250);
+            CrecimientoMaximo.X = random.Next(1, 150);
         }
 
         public new void Nacer()
         {
             Thread Proceso;
-            Color ColorAleatorio;
+            Color ColorVegetal;
             int X;
             int Y;
 
-            // Ser vivo obtiene su color
-            ColorAleatorio = ColorUtils.GenerateColorShade("green");
-
-            // El Vegetal nace después de un tiempo
-
+            // Vegetal obtiene su color
+            ColorVegetal = ColorUtils.GenerateColorShade("green");
+            
             Proceso = new Thread(() =>
             {
-                BackColor = ColorAleatorio;
+                BackColor = ColorVegetal;
                 Nacio = true;
 
-                // El vegetal escoge un punto aleatorio para nacer
-                X = random.Next(10, AreaDesplazamiento.Width - Size.Width);
-                Y = random.Next(10, AreaDesplazamiento.Height - Size.Height);
+                // El vegetal tiene un punto aleatorio para nacer
+                X = random.Next(10, AreaDesplazamiento.Width - CrecimientoMaximo.X);
+                Y = random.Next(10, AreaDesplazamiento.Height - CrecimientoMaximo.Y);
 
                 // Aplicar la posición
                 Location = new Point(X, Y);
@@ -60,45 +65,59 @@ namespace POO22B_MZJA.src.Clases
             int W;
             int H;
 
-            bool CrecimientoEquitativo;
-
-            CrecimientoEquitativo = new RandomEx().NextBoolean();
-
             W = 0;
             H = 0;
 
-            // Definir del crecimiento máximo del vegetal de forma aleatoria
-            CrecimientoMaximo.X = random.Next(1, 150);
-            CrecimientoMaximo.Y = random.Next(1, 150);
-
             // Definir tasa de crecimiento
-            TasaCrecimiento = random.Next(1, 20);
+            TasaCrecimiento = random.Next(1, 10);
 
-            ProcesoCrecimiento = new Thread( () =>
+            ProcesoCrecimiento = new Thread(() =>
             {
-                while (!Crecido)
+                while(!Muerto)
                 {
-
-                    Thread.Sleep(1000);
-
-                    if (Width > CrecimientoMaximo.X &&
-                        Height > CrecimientoMaximo.Y)
+                    while (!Crecido)
                     {
-                        Crecido = true;
+                        Thread.Sleep(1000);
+
+                        if (Width > CrecimientoMaximo.X &&
+                            Height > CrecimientoMaximo.Y)
+                        {
+                            Crecido = true;
+                        }
+
+                        W += TasaCrecimiento;
+                        H += TasaCrecimiento;
+
+                        Width += W;
+                        Height += H;
+
                     }
-
-
-
-                    W += TasaCrecimiento;
-                    H += TasaCrecimiento;
-
-                    Width += W;
-                    Height += H;
-
                 }
             });
 
             ProcesoCrecimiento.Start();
+            
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+            Graphics g;
+            GraphicsPath GPath;
+
+            g = pe.Graphics;
+
+            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            GPath = new GraphicsPath();
+
+            GPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+
+            Region = new Region(GPath);
 
         }
 
