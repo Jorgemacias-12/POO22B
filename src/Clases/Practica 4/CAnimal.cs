eea9d9443ad093f97eac401649f7bd70cba53920
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-10-24 00:35:40 -0500

    Docs - agregando documentación
    	Añadiendo documentación a la clase Animal

--- src/Clases/Practica 4/CAnimal.cs
+++ src/Clases/Practica 4/CAnimal.cs
@@ -10,31 +10,56 @@ using System.Windows.Forms;
 
 namespace POO22B_MZJA.src.Clases.Practica_4
 {
+    // +------------------------------------------------------------------+
+    // |  Clase que representa un ser vivo                                |
+    // |  MZJA 29/09/22.                                                  |
+    // +------------------------------------------------------------------+
     public class CAnimal : CSerVivo
     {
+
+        // +------------------------------------------------------------------+
+        // |  Atributos                                                       |
+        // +------------------------------------------------------------------+
+
+        // +------------------------------------------------------------------+
+        // |  Constructor                                                     |
+        // +------------------------------------------------------------------+
         public CAnimal(Control AreaDesplazamiento, int XNacimiento, int YNacimiento, 
                        int NivelOxigeno, bool HaySol, int LimiteInanicion) : 
                base(AreaDesplazamiento, XNacimiento, YNacimiento, NivelOxigeno, HaySol, LimiteInanicion)
         {
         }
 
+        // +------------------------------------------------------------------+
+        // |  Sobrecarga del método Desplazar.                                |
+        // +------------------------------------------------------------------+
         public override void Desplazar(int Velocidad)
         {
             base.Desplazar(Velocidad);
             base.Cansarse();
         }
 
+        // +------------------------------------------------------------------+
+        // |  Sobreescribir el método de la clase padre.                      |
+        // +------------------------------------------------------------------+
         public override void EnClic(object sender, EventArgs e)
         {
             base.Comer();
         }
 
+        // +------------------------------------------------------------------+
+        // |  Sobreescribir el funcionamiento de nacer de la clase padre      |
+        // |  para incluir generación de tipo.                                |
+        // +------------------------------------------------------------------+
         public override void Nacer()
         {
             base.Nacer();
             GenerarTipo();
         }
 
+        // +------------------------------------------------------------------+
+        // |  Generar el tipo según la clase Animal.                          | 
+        // +------------------------------------------------------------------+
         protected override void GenerarTipo()
         {
             int IndiceGenerado;
