9e026eddab26eee6f4bcacaa4e3363b9d2e90bb4
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-10-24 15:52:11 -0500

    Add - clase bacteria
    	Falta implementar funcion en clic

--- src/Clases/Practica 4/CBacteria.cs
+++ src/Clases/Practica 4/CBacteria.cs
@@ -3,149 +3,79 @@ using POO22B_MZJA.src.Utils.Rand;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace POO22B_MZJA.src.Clases
+namespace POO22B_MZJA.src.Clases.Practica_4
 {
+    // +------------------------------------------------------------------+
+    // |  Clase que representa una Bacteria                               |
+    // |  MZJA 29/09/22.                                                  |
+    // +------------------------------------------------------------------+
     public class CBacteria : CSerVivo
     {
-        private Random random = new Random();
+        // +------------------------------------------------------------------+
+        // |  Atributos                                                       |
+        // +------------------------------------------------------------------+
+        private List<Image> Bacterias;
 
-        public CBacteria(Control AreaDesplazamiento, int XNacimiento, int YNacimiento) :
-            base(AreaDesplazamiento, XNacimiento, YNacimiento)
+        // +------------------------------------------------------------------+
+        // |  Constructor                                                     |
+        // +------------------------------------------------------------------+
+        public CBacteria(Control AreaDesplazamiento, int XNacimiento, int YNacimiento, int NivelOxigeno, 
+                         bool HaySol, int LimiteInanicion) : 
+               base(AreaDesplazamiento, XNacimiento, YNacimiento, NivelOxigeno, HaySol, LimiteInanicion)
+        {
+            // Inicializar lista con rescursos graficos
+            Bacterias = new List<Image>()
+            {
+                Resources.mc_bacteria,
+                Resources.mc_bacteria_2
+            };
+        }
+
+
+        // +------------------------------------------------------------------+
+        // |  La bacteria se mueve de manera erratica en el ambiente.         |
+        // +------------------------------------------------------------------+
+        public override void Desplazar(int Velocidad)
+        {
+            base.Desplazar(Velocidad);
+        }
+
+        // +------------------------------------------------------------------+
+        // |  Falta implementar funcionalidad                                 |
+        // +------------------------------------------------------------------+
+        public override void EnClic(object sender, EventArgs e)
         {
         }
 
+        // +------------------------------------------------------------------+
+        // |  Incluye el método que genera el tipo de bacteria de manera      |
+        // |  aleatoria (pseudo)                                              |
+        // +------------------------------------------------------------------+
+        public override void Nacer()
+        {
+            base.Nacer();
+            GenerarTipo();
+        }
+
+        // +------------------------------------------------------------------+
+        // |  Generar el tipo según la clase Bacteria                         | 
+        // +------------------------------------------------------------------+
         protected override void GenerarTipo()
         {
-            int GeneratedIndex;
+            int IndiceGenerado;
 
-            List<Image> Mushroom = new List<Image>()
-            {
-                Resources.mc_bacteria,
-                Resources.mc_bacteria_2,
-            };
-
-            GeneratedIndex = RandomIC.Next(0, 1);
+            IndiceGenerado = RandomIC.Next(0, 1);
 
             FlatAppearance.BorderSize = 0;
             BackColor = Color.Transparent;
-            BackgroundImage = Mushroom[GeneratedIndex];
+
+            BackgroundImage = Bacterias[IndiceGenerado];
             BackgroundImageLayout = ImageLayout.Stretch;
         }
-
-        public override void Nacer(int LimiteInanicion, ref int NivelOxigeno)
-        {
-            Thread Proceso;
-            int X;
-            int Y;
-
-            // Comprobar si hay suficiente oxigeno
-            if (NivelOxigeno < 0 ||
-                NivelOxigeno > 100)
-            {
-                // El ser vivo consume oxigeno al nacer
-                NivelOxigeno -= 1;
-            }
-
-            // Vegetal obtiene una imagen de vegetal aleatoria
-            GenerarTipo();
-
-            Crecer();
-
-            Proceso = new Thread(() =>
-            {
-                Nacio = true;
-
-                // El vegetal tiene un punto aleatorio para nacer
-                X = random.Next(10, AreaDesplazamiento.Width - Width);
-                Y = random.Next(10, AreaDesplazamiento.Height - Height);
-
-                // Aplicar la posición
-                Location = new Point(X, Y);
-
-            });
-
-            Proceso.Start();
-        }
-
-        public override void Desplazar(int Velocidad)
-        {
-            // Variables de posición inicial
-            int X;
-            int Y;
-
-            this.Velocidad = Velocidad;
-
-            // Desplaza la particula por tiempo indefinido
-            // NOTA: Se utiliza un hilo de ejecución, el cual deberá
-            // ser  finalizazdo al término del programa.
-
-            ProcesoVida = new Thread(() =>
-            {
-                while (!Muerto)
-                {
-                    if (Nacio)
-                    {
-
-                        // Posición inical.
-                        X = Location.X;
-                        Y = Location.Y;
-
-                        // Calcula desplazamiento
-
-                        if (random.Next(0, 2) == 0)
-                        {
-                            Norte = true;
-                            Sur = false;
-                        }
-                        else
-                        {
-                            Norte = false;
-                            Sur = true;
-                        }
-
-                        if (random.Next(0, 2) == 0)
-                        {
-                            Este = true;
-                            Oeste = false;
-                        }
-                        else
-                        {
-                            Este = false;
-                            Oeste = true;
-                        }
-
-                        if (Norte)
-                        {
-                            Y -= 1;
-                        }
-
-                        if (Sur)
-                        {
-                            Y += 1;
-                        }
-
-                        if (Este)
-                        {
-                            X += 1;
-                        }
-
-                        if (Oeste)
-                        {
-                            X -= 1;
-                        }
-
-                        // Posición final 
-                        Location = new Point(X, Y);
-                        Thread.Sleep(this.Velocidad);
-                    }
-                }
-            });
-
-            ProcesoVida.Start();
-        }
-
     }
 }
