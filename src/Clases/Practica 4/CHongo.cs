1047638811ca694c3b3f4f5e8091ca15ccfb58e9
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-10-24 06:43:32 -0500

    Add - Implementacion terminada
    	Falta documentación y terminar implmenetación método abstracto

--- src/Clases/Practica 4/CHongo.cs
+++ src/Clases/Practica 4/CHongo.cs
@@ -1,27 +1,56 @@
-﻿using System.Windows.Forms;
+﻿using POO22B_MZJA.Properties;
+using POO22B_MZJA.src.Utils.Rand;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace POO22B_MZJA.src.Clases
+namespace POO22B_MZJA.src.Clases.Practica_4
 {
-    // +------------------------------------------------------------------+
-    // |  Clase que representa un ser vivo                                |
-    // |  MZJA 29/09/22.                                                  |
-    // +------------------------------------------------------------------+
     public class CHongo : CSerVivo
     {
+        private List<Image> Hongos;
 
-        // +------------------------------------------------------------------+
-        // |  Atributos                                                       |
-        // +------------------------------------------------------------------+
-
-
-        // +------------------------------------------------------------------+
-        // |  Constructor                                                     |
-        // +------------------------------------------------------------------+
-        public CHongo(Control AreaDesplazamiento, int XNacimiento, int YNacimiento) :
-            base(AreaDesplazamiento, XNacimiento, YNacimiento)
+        public CHongo(Control AreaDesplazamiento, int XNacimiento, int YNacimiento, 
+                      int NivelOxigeno, bool HaySol, int LimiteInanicion) : 
+               base(AreaDesplazamiento, XNacimiento, YNacimiento, NivelOxigeno, HaySol, LimiteInanicion)
         {
+            Hongos = new List<Image>()
+            {
+                Resources.mc_mushroom_red,
+                Resources.mc_mushroom_brown,
+                Resources.gi_hongo,
+                Resources.gi_hongo_estelar,
+                Resources.gi_hongo_rukkhashava
+            };
+        }
+
+        public override void EnClic(object sender, EventArgs e)
+        { 
 
         }
 
+        public override void Nacer()
+        {
+            base.Nacer();
+            GenerarTipo();
+        }
+
+        protected override void GenerarTipo()
+        {
+            int IndiceGenerado;
+
+            IndiceGenerado = RandomIC.Next(0, 4);
+
+            FlatAppearance.BorderSize = 0;
+            BackColor = Color.Transparent;
+
+            BackgroundImage = Hongos[IndiceGenerado];
+            BackgroundImageLayout = ImageLayout.Stretch;
+
+        }
     }
 }
