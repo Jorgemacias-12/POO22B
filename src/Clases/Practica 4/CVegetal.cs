2875747d9d50ef430246ef3ba0f096cfbcb2408c
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-10-24 03:31:30 -0500

    Add - re implementación terminada
    	Falta documentar

--- src/Clases/Practica 4/CVegetal.cs
+++ src/Clases/Practica 4/CVegetal.cs
@@ -1,47 +1,29 @@
 ﻿using POO22B_MZJA.Properties;
+using POO22B_MZJA.src.Componentes.CMessageBox;
 using POO22B_MZJA.src.Utils.Rand;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace POO22B_MZJA.src.Clases
+namespace POO22B_MZJA.src.Clases.Practica_4
 {
     public class CVegetal : CSerVivo
     {
 
-        private Point CrecimientoMaximo;
-        private readonly Random random;
-        private bool Crecido;
-        private int TasaCrecimiento;
+        private List<Image> Vegetales;
+        private List<Image> Frutos;
+        private List<string> NombreFrutos;
 
-        public CVegetal(Control AreaDesplazamiento, int XNacimiento, int YNacimiento) :
-            base(AreaDesplazamiento, XNacimiento, YNacimiento)
+        public CVegetal(Control AreaDesplazamiento, int XNacimiento, int YNacimiento,
+                        int NivelOxigeno, bool HaySol, int LimiteInanicion) :
+               base(AreaDesplazamiento, XNacimiento, YNacimiento, NivelOxigeno, HaySol, LimiteInanicion)
         {
-            random = new Random();
-            // Eliminar borde
-            FlatAppearance.BorderSize = 0;
-
-            // Definir crecimiento máximo del vegetal
-            CrecimientoMaximo.X = random.Next(1, 75);
-            CrecimientoMaximo.X = random.Next(1, 75);
-
-            Click += CVegetal_Click;
-
-        }
-
-        private void CVegetal_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        protected override void GenerarTipo()
-        {
-            int RandomIndex;
-
-            List<Image> Vegetables = new List<Image>()
+            // Inicializar recursos graficos
+            Vegetales = new List<Image>()
             {
                 Resources.mc_acacia_tr,
                 Resources.mc_birch_tr,
@@ -50,107 +32,65 @@ namespace POO22B_MZJA.src.Clases
                 Resources.mc_spruce_tr,
             };
 
-            RandomIndex = RandomIC.Next(0, 4);
+            Frutos = new List<Image>()
+            {
+                Resources.mc_fruit_apple,
+                Resources.mc_fruit_blueberry,
+                Resources.mc_fruit_gapple,
+                Resources.mc_fruit_orange,
+                Resources.mc_fruit_sweet_berries
+            };
 
+            NombreFrutos = new List<string>()
+            {
+                "Apple",
+                "Blueberry",
+                "Green Apple",
+                "Orange",
+                "Sweet Berries"
+            };
+
+            //Frutos = new Dictionary<string, Image>()
+            //{
+            //    {"Apple", Resources.mc_fruit_apple},
+            //    {"BlueBerry", Resources.mc_fruit_blueberry},
+            //    {"Green Apple", Resources.mc_fruit_blueberry},
+            //    {"Orange", Resources.mc_fruit_orange },
+            //    {"Sweet Berries", Resources.mc_fruit_sweet_berries}
+            //};
+
+            
+
+        }
+
+        public override void EnClic(object sender, EventArgs e)
+        {
+            int IndiceGenerado;
+
+            IndiceGenerado = RandomIC.Next(0, 4);
+
+            new DialogFrame().Show("¡Atención!",$"Se ha recogido la fruta: {NombreFrutos[IndiceGenerado]}", Frutos[IndiceGenerado]);
+        }
+
+        public override void Nacer()
+        {
+            base.Nacer();
+            GenerarTipo();
+        }
+
+        protected override void GenerarTipo()
+        {
+            int IndiceGenerado;
+
+
+
+            IndiceGenerado = RandomIC.Next(0, 4);
+
+            FlatAppearance.BorderSize = 0;
             BackColor = Color.Transparent;
-            BackgroundImage = Vegetables[RandomIndex];
+
+            BackgroundImage = Vegetales[IndiceGenerado];
             BackgroundImageLayout = ImageLayout.Stretch;
         }
-
-        public override void Nacer(int LimiteInanicion, ref int NivelOxigeno)
-        {
-            Thread Proceso;
-            int X;
-            int Y;
-
-            // Comprobar si hay suficiente oxigeno
-            if (NivelOxigeno < 0 ||
-                NivelOxigeno > 100) return;
-
-            // El ser vivo consume oxigeno al nacer
-            NivelOxigeno -= 1;
-
-            // Vegetal obtiene una imagen de vegetal aleatoria
-            GenerarTipo();
-
-            Proceso = new Thread(() =>
-            {
-                Nacio = true;
-
-                // El vegetal tiene un punto aleatorio para nacer
-                X = random.Next(10, AreaDesplazamiento.Width - CrecimientoMaximo.X - Width);
-                Y = random.Next(10, AreaDesplazamiento.Height - CrecimientoMaximo.Y - Height);
-
-                // Aplicar la posición
-                Location = new Point(X, Y);
-
-            });
-
-            Proceso.Start();
-        }
-
-        public override void Crecer()
-        {
-            Thread ProcesoCrecimiento;
-
-            int W;
-            int H;
-
-            W = 0;
-            H = 0;
-
-            // Definir tasa de crecimiento
-            TasaCrecimiento = random.Next(1, 5);
-
-            ProcesoCrecimiento = new Thread(() =>
-            {
-                while (!Muerto)
-                {
-                    while (!Crecido)
-                    {
-                        Thread.Sleep(1000);
-
-                        if (Width > CrecimientoMaximo.X &&
-                            Height > CrecimientoMaximo.Y)
-                        {
-                            Crecido = true;
-                        }
-
-                        W += TasaCrecimiento;
-                        H += TasaCrecimiento;
-
-                        Width += W;
-                        Height += H;
-
-                    }
-                }
-            });
-
-            ProcesoCrecimiento.Start();
-
-        }
-
-        protected override void OnPaint(PaintEventArgs pe)
-        {
-            base.OnPaint(pe);
-
-            Graphics g;
-            GraphicsPath GPath;
-
-            g = pe.Graphics;
-
-            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
-            g.CompositingQuality = CompositingQuality.HighQuality;
-            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
-            g.SmoothingMode = SmoothingMode.AntiAlias;
-
-            GPath = new GraphicsPath();
-
-            GPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
-
-            Region = new Region(GPath);
-
-        }
-
     }
 }
