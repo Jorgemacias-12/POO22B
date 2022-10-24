73aefb3229212e392e13878291210a49f55351e9
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-10-24 03:24:27 -0500

    Add - Componente requerido para CVegetal

---
+++ src/Componentes/CMessageBox/DialogFrame.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace POO22B_MZJA.src.Componentes.CMessageBox
+{
+    partial class DialogFrame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PnlContainer = new System.Windows.Forms.Panel();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.LblMensaje = new System.Windows.Forms.Label();
+            this.PbxIcon = new System.Windows.Forms.PictureBox();
+            this.PnlContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PbxIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PnlContainer
+            // 
+            this.PnlContainer.Controls.Add(this.BtnClose);
+            this.PnlContainer.Controls.Add(this.LblMensaje);
+            this.PnlContainer.Controls.Add(this.PbxIcon);
+            this.PnlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PnlContainer.Location = new System.Drawing.Point(0, 0);
+            this.PnlContainer.Name = "PnlContainer";
+            this.PnlContainer.Size = new System.Drawing.Size(372, 114);
+            this.PnlContainer.TabIndex = 0;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Location = new System.Drawing.Point(285, 79);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(75, 23);
+            this.BtnClose.TabIndex = 2;
+            this.BtnClose.Text = "Aceptar";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // LblMensaje
+            // 
+            this.LblMensaje.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMensaje.Location = new System.Drawing.Point(82, 24);
+            this.LblMensaje.Name = "LblMensaje";
+            this.LblMensaje.Size = new System.Drawing.Size(278, 34);
+            this.LblMensaje.TabIndex = 1;
+            this.LblMensaje.Text = "Texto de ejemplo";
+            // 
+            // PbxIcon
+            // 
+            this.PbxIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PbxIcon.Location = new System.Drawing.Point(12, 24);
+            this.PbxIcon.Name = "PbxIcon";
+            this.PbxIcon.Size = new System.Drawing.Size(64, 64);
+            this.PbxIcon.TabIndex = 0;
+            this.PbxIcon.TabStop = false;
+            // 
+            // DialogFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 114);
+            this.ControlBox = false;
+            this.Controls.Add(this.PnlContainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "DialogFrame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DialogFrame";
+            this.PnlContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PbxIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlContainer;
+        private System.Windows.Forms.PictureBox PbxIcon;
+        private System.Windows.Forms.Label LblMensaje;
+        private System.Windows.Forms.Button BtnClose;
+    }
+}
---
+++ src/Componentes/CMessageBox/DialogFrame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO22B_MZJA.src.Componentes.CMessageBox
+{
+    public partial class DialogFrame : Form
+    {
+
+        public string Mensaje;
+        public Image Imagen;
+
+        public DialogFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Dispose();
+        }
+
+        public void Show(string Caption,string Mensaje, Image Imagen)
+        {
+            this.Text = Caption;
+
+            this.Mensaje = Mensaje;
+            this.Imagen = Imagen;
+
+            PbxIcon.BackgroundImage = Imagen;
+            LblMensaje.Text = Mensaje;
+
+            this.ShowDialog();
+
+        }
+
+    }
+}
