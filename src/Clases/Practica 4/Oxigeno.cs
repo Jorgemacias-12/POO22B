b1f029832ffe43074034403c355f6c3759c887f8
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-10-25 10:16:42 -0500

    Add - Sistema de oxigeno para ecosistema
    	Pendiente de integrarse con la GUI

---
+++ src/Clases/Practica 4/Oxigeno.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace POO22B_MZJA.src.Clases.Practica_4
+{
+    public class Oxigeno
+    {
+
+        public int CapacidadMaxima
+        {
+            get; set;
+        }
+
+        public int ValorActual
+        {
+            get; set;
+        }
+
+        private Thread ProcesoOxigeno;
+        private bool Ejecutar;
+       
+
+        public Oxigeno(int CapacidadMaxima)
+        {
+            this.CapacidadMaxima = CapacidadMaxima;
+            Ejecutar = true;
+            ValorActual = CapacidadMaxima;
+        }
+
+        public void CicloOxigeno()
+        {
+            ProcesoOxigeno = new Thread(() =>
+            {
+                while(Ejecutar)
+                {
+
+                }
+            });
+
+            ProcesoOxigeno.Start();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
