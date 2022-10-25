97de75eb51b468c41cc0756b2aa59b455a937186
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-10-25 10:15:20 -0500

    AD - eliminar clase antigua y añadir cambios

--- src/Clases/Practica 4/CHomoSapien.cs
+++ src/Clases/Practica 4/CHomoSapien.cs
@@ -1,12 +1,16 @@
-﻿using System.Windows.Forms;
-namespace POO22B_MZJA.src.Clases
-{
-    public class CHomoSapiens : CHominidae
-    {
-        public CHomoSapiens(Control AreaDesplazamiento, int XNacimiento, int YNacimiento) :
-            base(AreaDesplazamiento, XNacimiento, YNacimiento)
-        {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
+namespace POO22B_MZJA.src.Clases.Practica_4
+{
+    public class CHomoSapien : CHominidae
+    {
+        public CHomoSapien(Control AreaDesplazamiento, int XNacimiento, int YNacimiento, int NivelOxigeno, bool HaySol, int LimiteInanicion) : base(AreaDesplazamiento, XNacimiento, YNacimiento, NivelOxigeno, HaySol, LimiteInanicion)
+        {
         }
     }
 }
