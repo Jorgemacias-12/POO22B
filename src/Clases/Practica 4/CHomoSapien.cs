db04dc7dc573e56f65401854325f39fcf63d5669
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-10-28 03:33:58 -0500

    Add - Me falto esta clase PUTA MADRE

--- src/Clases/Practica 4/CHomoSapien.cs
+++ src/Clases/Practica 4/CHomoSapien.cs
@@ -9,7 +9,7 @@ namespace POO22B_MZJA.src.Clases.Practica_4
 {
     public class CHomoSapien : CHominidae
     {
-        public CHomoSapien(Control AreaDesplazamiento, int XNacimiento, int YNacimiento, int NivelOxigeno, bool HaySol, int LimiteInanicion) : base(AreaDesplazamiento, XNacimiento, YNacimiento, NivelOxigeno, HaySol, LimiteInanicion)
+        public CHomoSapien(Control AreaDesplazamiento, int XNacimiento, int YNacimiento, Oxigeno Oxigeno, bool HaySol, int LimiteInanicion) : base(AreaDesplazamiento, XNacimiento, YNacimiento, Oxigeno, HaySol, LimiteInanicion)
         {
         }
     }
