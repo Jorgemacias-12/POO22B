e371fe547db86ddae0827c21a247badb2e5fc1d4
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-11-15 05:21:42 -0600

    Add - Clase intermediaria #13
    	He creado esta clase como un intermediario encargado de
    	manejar las acciones de las figuras.

---
+++ src/Clases/Practica 5/CFigureManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO22B_MZJA.src.Clases.Practica_5
+{
+    // +------------------------------------------------------------------+
+    // |  Clase encargada del manejo de las figuras                       |
+    // |  MZJA 01/09/22.                                                  |
+    // +------------------------------------------------------------------+
+    public class CFigureManager
+    {
+        // +------------------------------------------------------------------+
+        // |  Atributos                                                       |
+        // +------------------------------------------------------------------+
+
+        // +------------------------------------------------------------------+
+        // |  Constructor                                                     |
+        // +------------------------------------------------------------------+
+        public CFigureManager()
+        {
+
+        }
+    }
+}
