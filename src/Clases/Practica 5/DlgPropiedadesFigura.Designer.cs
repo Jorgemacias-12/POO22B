b819f413265c0a6602951644b04dce621485335a
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-11-15 12:13:26 -0600

    Add - Dialogo de propiedades
    	He implementado este diálogo como intermediario buscando que el
    	código no se repita demasiado.

---
+++ src/Clases/Practica 5/DlgPropiedadesFigura.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace POO22B_MZJA.src.Clases.Practica_5
+{
+    partial class DlgPropiedadesFigura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PnlContainer = new System.Windows.Forms.Panel();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.LblAltura = new System.Windows.Forms.Label();
+            this.LblBase = new System.Windows.Forms.Label();
+            this.LblRadio = new System.Windows.Forms.Label();
+            this.TbxPerimeterSize = new System.Windows.Forms.TextBox();
+            this.LblPerimetroGrosor = new System.Windows.Forms.Label();
+            this.BtnPColor = new System.Windows.Forms.Button();
+            this.BtnAColor = new System.Windows.Forms.Button();
+            this.TbxAncho = new System.Windows.Forms.TextBox();
+            this.TbxAlto = new System.Windows.Forms.TextBox();
+            this.LblFiguraAncho = new System.Windows.Forms.Label();
+            this.LblFiguraAlto = new System.Windows.Forms.Label();
+            this.LblOperation = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.PnlContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnlContainer
+            // 
+            this.PnlContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(36)))));
+            this.PnlContainer.Controls.Add(this.textBox3);
+            this.PnlContainer.Controls.Add(this.textBox2);
+            this.PnlContainer.Controls.Add(this.textBox1);
+            this.PnlContainer.Controls.Add(this.BtnGuardar);
+            this.PnlContainer.Controls.Add(this.LblAltura);
+            this.PnlContainer.Controls.Add(this.LblBase);
+            this.PnlContainer.Controls.Add(this.LblRadio);
+            this.PnlContainer.Controls.Add(this.TbxPerimeterSize);
+            this.PnlContainer.Controls.Add(this.LblPerimetroGrosor);
+            this.PnlContainer.Controls.Add(this.BtnPColor);
+            this.PnlContainer.Controls.Add(this.BtnAColor);
+            this.PnlContainer.Controls.Add(this.TbxAncho);
+            this.PnlContainer.Controls.Add(this.TbxAlto);
+            this.PnlContainer.Controls.Add(this.LblFiguraAncho);
+            this.PnlContainer.Controls.Add(this.LblFiguraAlto);
+            this.PnlContainer.Controls.Add(this.LblOperation);
+            this.PnlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PnlContainer.Location = new System.Drawing.Point(0, 0);
+            this.PnlContainer.Margin = new System.Windows.Forms.Padding(4);
+            this.PnlContainer.Name = "PnlContainer";
+            this.PnlContainer.Size = new System.Drawing.Size(784, 561);
+            this.PnlContainer.TabIndex = 0;
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.BackColor = System.Drawing.Color.Coral;
+            this.BtnGuardar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BtnGuardar.FlatAppearance.BorderSize = 0;
+            this.BtnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGuardar.ForeColor = System.Drawing.Color.Black;
+            this.BtnGuardar.Location = new System.Drawing.Point(0, 512);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(784, 49);
+            this.BtnGuardar.TabIndex = 14;
+            this.BtnGuardar.Text = "Guardar propiedades";
+            this.BtnGuardar.UseVisualStyleBackColor = false;
+            this.BtnGuardar.Click += new System.EventHandler(this.BtnGuardar_Click);
+            // 
+            // LblAltura
+            // 
+            this.LblAltura.AutoSize = true;
+            this.LblAltura.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAltura.ForeColor = System.Drawing.Color.Coral;
+            this.LblAltura.Location = new System.Drawing.Point(15, 260);
+            this.LblAltura.Name = "LblAltura";
+            this.LblAltura.Size = new System.Drawing.Size(169, 17);
+            this.LblAltura.TabIndex = 13;
+            this.LblAltura.Text = "Altura del triangulo:";
+            // 
+            // LblBase
+            // 
+            this.LblBase.AutoSize = true;
+            this.LblBase.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBase.ForeColor = System.Drawing.Color.Coral;
+            this.LblBase.Location = new System.Drawing.Point(15, 223);
+            this.LblBase.Name = "LblBase";
+            this.LblBase.Size = new System.Drawing.Size(157, 17);
+            this.LblBase.TabIndex = 12;
+            this.LblBase.Text = "Base del triangulo:";
+            // 
+            // LblRadio
+            // 
+            this.LblRadio.AutoSize = true;
+            this.LblRadio.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRadio.ForeColor = System.Drawing.Color.Coral;
+            this.LblRadio.Location = new System.Drawing.Point(14, 189);
+            this.LblRadio.Name = "LblRadio";
+            this.LblRadio.Size = new System.Drawing.Size(145, 17);
+            this.LblRadio.TabIndex = 11;
+            this.LblRadio.Text = "Radio del circulo:";
+            // 
+            // TbxPerimeterSize
+            // 
+            this.TbxPerimeterSize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
+            this.TbxPerimeterSize.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TbxPerimeterSize.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbxPerimeterSize.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.TbxPerimeterSize.Location = new System.Drawing.Point(198, 147);
+            this.TbxPerimeterSize.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TbxPerimeterSize.MaxLength = 3;
+            this.TbxPerimeterSize.Name = "TbxPerimeterSize";
+            this.TbxPerimeterSize.Size = new System.Drawing.Size(104, 24);
+            this.TbxPerimeterSize.TabIndex = 10;
+            this.TbxPerimeterSize.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TbxPerimeterSize_KeyPress);
+            this.TbxPerimeterSize.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TbxPerimeterSize_KeyUp);
+            // 
+            // LblPerimetroGrosor
+            // 
+            this.LblPerimetroGrosor.AutoSize = true;
+            this.LblPerimetroGrosor.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPerimetroGrosor.ForeColor = System.Drawing.Color.Coral;
+            this.LblPerimetroGrosor.Location = new System.Drawing.Point(13, 149);
+            this.LblPerimetroGrosor.Name = "LblPerimetroGrosor";
+            this.LblPerimetroGrosor.Size = new System.Drawing.Size(179, 17);
+            this.LblPerimetroGrosor.TabIndex = 9;
+            this.LblPerimetroGrosor.Text = "Tamaño del perimetro:";
+            // 
+            // BtnPColor
+            // 
+            this.BtnPColor.BackColor = System.Drawing.Color.White;
+            this.BtnPColor.FlatAppearance.BorderSize = 0;
+            this.BtnPColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnPColor.Location = new System.Drawing.Point(167, 296);
+            this.BtnPColor.Name = "BtnPColor";
+            this.BtnPColor.Size = new System.Drawing.Size(137, 49);
+            this.BtnPColor.TabIndex = 8;
+            this.BtnPColor.Text = "Color Perimetro";
+            this.BtnPColor.UseVisualStyleBackColor = false;
+            this.BtnPColor.Click += new System.EventHandler(this.BtnPColor_Click);
+            // 
+            // BtnAColor
+            // 
+            this.BtnAColor.BackColor = System.Drawing.Color.White;
+            this.BtnAColor.FlatAppearance.BorderSize = 0;
+            this.BtnAColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAColor.Location = new System.Drawing.Point(15, 296);
+            this.BtnAColor.Name = "BtnAColor";
+            this.BtnAColor.Size = new System.Drawing.Size(143, 49);
+            this.BtnAColor.TabIndex = 7;
+            this.BtnAColor.Text = "Color Area";
+            this.BtnAColor.UseVisualStyleBackColor = false;
+            this.BtnAColor.Click += new System.EventHandler(this.BtnAColor_Click);
+            // 
+            // TbxAncho
+            // 
+            this.TbxAncho.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
+            this.TbxAncho.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TbxAncho.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbxAncho.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.TbxAncho.Location = new System.Drawing.Point(180, 111);
+            this.TbxAncho.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TbxAncho.MaxLength = 4;
+            this.TbxAncho.Name = "TbxAncho";
+            this.TbxAncho.Size = new System.Drawing.Size(122, 24);
+            this.TbxAncho.TabIndex = 6;
+            this.TbxAncho.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TbxAncho_KeyPress);
+            this.TbxAncho.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TbxAncho_KeyUp);
+            // 
+            // TbxAlto
+            // 
+            this.TbxAlto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
+            this.TbxAlto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TbxAlto.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbxAlto.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.TbxAlto.Location = new System.Drawing.Point(165, 70);
+            this.TbxAlto.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TbxAlto.MaxLength = 4;
+            this.TbxAlto.Name = "TbxAlto";
+            this.TbxAlto.Size = new System.Drawing.Size(137, 24);
+            this.TbxAlto.TabIndex = 5;
+            this.TbxAlto.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TbxAlto_KeyPress);
+            this.TbxAlto.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TbxAlto_KeyUp);
+            // 
+            // LblFiguraAncho
+            // 
+            this.LblFiguraAncho.AutoSize = true;
+            this.LblFiguraAncho.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblFiguraAncho.ForeColor = System.Drawing.Color.Coral;
+            this.LblFiguraAncho.Location = new System.Drawing.Point(12, 110);
+            this.LblFiguraAncho.Name = "LblFiguraAncho";
+            this.LblFiguraAncho.Size = new System.Drawing.Size(84, 17);
+            this.LblFiguraAncho.TabIndex = 4;
+            this.LblFiguraAncho.Text = "Ancho del";
+            // 
+            // LblFiguraAlto
+            // 
+            this.LblFiguraAlto.AutoSize = true;
+            this.LblFiguraAlto.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblFiguraAlto.ForeColor = System.Drawing.Color.Coral;
+            this.LblFiguraAlto.Location = new System.Drawing.Point(13, 72);
+            this.LblFiguraAlto.Name = "LblFiguraAlto";
+            this.LblFiguraAlto.Size = new System.Drawing.Size(69, 17);
+            this.LblFiguraAlto.TabIndex = 3;
+            this.LblFiguraAlto.Text = "Alto del";
+            // 
+            // LblOperation
+            // 
+            this.LblOperation.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LblOperation.Font = new System.Drawing.Font("Minecraftia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblOperation.ForeColor = System.Drawing.Color.Coral;
+            this.LblOperation.Location = new System.Drawing.Point(0, 0);
+            this.LblOperation.Name = "LblOperation";
+            this.LblOperation.Size = new System.Drawing.Size(784, 57);
+            this.LblOperation.TabIndex = 0;
+            this.LblOperation.Text = "Figura a pintar:";
+            this.LblOperation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.textBox1.Location = new System.Drawing.Point(167, 187);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.MaxLength = 3;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 24);
+            this.textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.textBox2.Location = new System.Drawing.Point(178, 221);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.MaxLength = 3;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 24);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.textBox3.Location = new System.Drawing.Point(190, 258);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.MaxLength = 3;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(114, 24);
+            this.textBox3.TabIndex = 17;
+            // 
+            // DlgPropiedadesFigura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.PnlContainer);
+            this.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "DlgPropiedadesFigura";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Propiedades de figura";
+            this.Load += new System.EventHandler(this.DlgPropiedadesFigura_Load);
+            this.PnlContainer.ResumeLayout(false);
+            this.PnlContainer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlContainer;
+        private System.Windows.Forms.Label LblOperation;
+        private System.Windows.Forms.Label LblFiguraAlto;
+        private System.Windows.Forms.Label LblFiguraAncho;
+        private System.Windows.Forms.TextBox TbxAlto;
+        private System.Windows.Forms.TextBox TbxAncho;
+        private System.Windows.Forms.Button BtnAColor;
+        private System.Windows.Forms.Button BtnPColor;
+        private System.Windows.Forms.Label LblPerimetroGrosor;
+        private System.Windows.Forms.TextBox TbxPerimeterSize;
+        private System.Windows.Forms.Label LblRadio;
+        private System.Windows.Forms.Label LblBase;
+        private System.Windows.Forms.Label LblAltura;
+        private System.Windows.Forms.Button BtnGuardar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
---
+++ src/Clases/Practica 5/DlgPropiedadesFigura.cs
@@ -0,0 +1,196 @@
+﻿using POO22B_MZJA.src.Utils;
+using POO22B_MZJA.src.Utils.GUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO22B_MZJA.src.Clases.Practica_5
+{
+    public partial class DlgPropiedadesFigura : Form
+    {
+        public int TipoFigura
+        {
+            get; set;
+        }
+
+        private int FiguraAlto;
+        private int FiguraAncho;
+        private int Base;
+        private int Altura;
+        private int Radio;
+        private int PerimeterSize;
+        private Color Perimetro;
+        private Color Area;
+
+        // Contiene el nombre de las figuras disponibles
+        public List<string> NombreFigura;
+
+        public DlgPropiedadesFigura(CFigureManager Manager)
+        {
+            InitializeComponent();
+
+            // Inicializar lista de valores
+            NombreFigura = new List<string>
+            {
+                "Cuadrado",
+                "Circulo",
+                "Triangulo",
+                "Rectangulo"
+            };
+
+            TipoFigura = Manager.TipoFigura;
+
+        }
+
+        public void ObtenerPropiedadesFigura(out int FiguraAlto,
+                                             out int FiguraAncho,
+                                             out Color Area,
+                                             out Color Perimetro,
+                                             out int PerimetroSize)
+        {
+            FiguraAlto = this.FiguraAlto;
+            FiguraAncho = this.FiguraAncho;
+            Area = this.Area;
+            Perimetro = this.Perimetro;
+            PerimetroSize = this.PerimeterSize;
+        }
+
+        public void ObtenerPropiedadesFigura(out int Base,
+                                             out int Altura,
+                                             out int PerimetroSize,
+                                             out Color Area,
+                                             out Color Perimetro)
+        {
+            Base = this.Base;
+            Altura = this.Altura;
+            PerimetroSize = this.PerimeterSize;
+            Area = this.Area;
+            Perimetro = this.Perimetro;
+        }
+
+        private void DlgPropiedadesFigura_Load(object sender, EventArgs e)
+        {
+            // Indicar figura seleccionada
+            this.Text += $" - {NombreFigura[TipoFigura]}";
+
+            // Mostrar tipo de figura en label
+            LblOperation.Text += $" {NombreFigura[TipoFigura]}";
+
+            // Cargar tipo de figura en caracteristicas
+            LblFiguraAlto.Text += $" {NombreFigura[TipoFigura]}: ";
+            LblFiguraAncho.Text += $" {NombreFigura[TipoFigura]}: ";
+        }
+
+        private void SetColorDialogForeground(object sender)
+        {
+            Button _ = sender as Button;
+
+            _.ForeColor = ColorUtils.GetForegroundColor(_.BackColor);
+        }
+
+        private void BtnAColor_Click(object sender, EventArgs e)
+        {
+            Area = ColorUtils.DialogColor();
+
+            BtnAColor.BackColor = Area;
+
+            SetColorDialogForeground(sender);
+        }
+
+        private void BtnPColor_Click(object sender, EventArgs e)
+        {
+            Perimetro = ColorUtils.DialogColor();
+
+            BtnPColor.BackColor = Perimetro;
+
+            SetColorDialogForeground(sender);
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            // Validar datos aquí
+
+            if (TbxAlto.Text == string.Empty ||
+                TbxAncho.Text == string.Empty)
+            {
+                MessageBox.Show("Llene los campos de altura y anchura para continuar",
+                                "¡Atención!",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+
+                return;
+            }
+
+            if (Area == Color.Empty ||
+                Perimetro == Color.Empty)
+            {
+                MessageBox.Show("Eliga un color valido para el perímetro y el área de la figura",
+                                "¡Atención!",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+
+                return;
+            }
+
+            if (PerimeterSize == 0)
+            {
+                MessageBox.Show("Eliga un valor númerico entre 1 y N para el grosor del perímetro",
+                                "¡Atención!",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+
+                return;
+            }
+
+            // Estado del diálogo exitoso!
+            DialogResult = DialogResult.OK;
+
+            Close();
+        }
+
+        private void TbxAlto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            UI.ValidateTextBox(sender, e);
+        }
+
+        private void TbxAlto_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (TbxAlto.Text != String.Empty)
+            {
+                FiguraAlto = Convert.ToInt32(TbxAlto.Text);
+            }
+        }
+
+        private void TbxAncho_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            UI.ValidateTextBox(sender, e);
+        }
+
+        private void TbxAncho_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (TbxAncho.Text != String.Empty)
+            {
+                FiguraAncho = Convert.ToInt32(TbxAncho.Text);
+            }
+        }
+
+        private void TbxPerimeterSize_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            UI.ValidateTextBox(sender, e);
+        }
+
+        private void TbxPerimeterSize_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (TbxPerimeterSize.Text != String.Empty)
+            {
+                PerimeterSize = Convert.ToInt32(TbxPerimeterSize.Text);
+            }
+        }
+    }
+}
